687ce8c6ccdf87a31b36e672ffeda0f09970f9df
Rosemo Mühendislik <35057450+rosemoengineering@users.noreply.github.com>
2019-08-11 22:39:22 +0300

    Add files via upload

---
+++ while_example.py
@@ -0,0 +1,21 @@
+sayi=0
+tek=0
+cift=0
+
+while sayi<=10:
+
+    if sayi%2==0:
+
+        cift+=1
+
+    else:
+
+        tek+=1
+
+    sayi+=1
+
+print('Tek sayıların adeti:', tek)
+
+print('Çift sayıların adeti:', cift)
+
+print('Sayının değeri:', sayi)
